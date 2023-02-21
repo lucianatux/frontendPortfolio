d773aa71cfd1d450b7fdab00135022d5cb7e0019
lucianatux <lucianatux@yahoo.com.ar>
2023-02-21 20:46:19 -0300

    Update environment.ts

--- PortfolioLCC/src/environments/environment.ts
+++ PortfolioLCC/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  URL: 'https://backendportfolio-production-f5d9.up.railway.app/'
 };
 
 /*
